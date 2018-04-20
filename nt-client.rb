7e348602067063d2be13da7a635137cc33fc2498
amg <amgoncalves@users.noreply.github.com>
2018-04-20 15:17:24 -0400

    updates client.

--- nt-client.rb
+++ nt-client.rb
@@ -12,6 +12,15 @@ tweet_id = "5ad9dd4fea73c95e24de8f0d"
 hash = nT.user.find_by_id(user_id)
 hash = nT.user.find_by_handle(user_handle)
 tweet_h = nT.tweet.find_by_id(tweet_id)
+recent_tweets = nT.tweet.find_most_recent()
+
+def print_tweets(tweets)
+  tweets.each do |num, tweet|
+    puts "#{num}: #{tweet["author_handle"]} tweeted: \"#{tweet["content"]}\" on #{tweet["time_created"]}"    
+  end
+end
 
 puts "#{hash["handle"]}'s email is: #{hash["email"]}" unless hash == nil
 puts "#{tweet_h["author_handle"]} tweeted: \"#{tweet_h["content"]}\" on #{tweet_h["time_created"]}" unless tweet_h == nil
+print_tweets(recent_tweets)
+
