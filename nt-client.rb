d42438aefffffb87fd37c09fc60cf5c937246bca
amg <amgoncalves@users.noreply.github.com>
2018-04-20 08:36:29 -0400

    Updates sample client program.

--- nt-client.rb
+++ nt-client.rb
@@ -2,11 +2,14 @@
 require './NanoTwitter.rb'
 require 'byebug'
 
-api_key = "mcurie"
+api_key = ENV['NT_ACCESS_TOKEN']
 
 nT = NanoTwitter.new(api_key)
 user_handle = "mcurie"
 user_id = "5acd336c31d664274e814839"
-hash = nT.find_user_by_id(user_id)
-#hash = nT.find_user_by_handle(user_handle)
+tweet_id = "5ad9dd4fea73c95e24de8f0d"
+hash = nT.user.find_by_id(user_id)
+hash = nT.user.find_by_handle(user_handle)
+tweet_h = nT.tweet.find_by_id(tweet_id)
 puts "#{hash["handle"]}'s email is: #{hash["email"]}" unless hash == nil
+puts "#{tweet_h["author_handle"]} tweeted: \"#{tweet_h["content"]}\" on #{tweet_h["time_created"]}" unless tweet_h == nil
